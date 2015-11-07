72c12d91af4af82faaeadafd869f81d64a1ae0a8
NicholasJDev <archangel.nick@yahoo.com>
2015-11-06 23:19:14 -0700

    FAQ file was added to the game, the necessary link added to the help menu executing part. The only left to fill it out with the information.

---
+++ greenGuardian/src/byui/cit260/greenguardian/view/FAQ.java
@@ -0,0 +1,31 @@
+/*********************************************************************
+    * Program: GreedGuardian, Developing the GameControl subclass 
+    * Brother: Jackson, CIT260 
+    * Author: Nicholas Balabanov 
+    * Summary: This is a GameControl class that responds for everything that 
+    * would happened in the game-program.
+    * **********************************************************************/
+package byui.cit260.greenguardian.view;
+
+/**
+ *
+ * @author nicholasjdev
+ */
+class FAQ {
+   final static void display()
+   {
+   System.out.println("/***************************************************************************\\");
+   System.out.print("*             Q1: How to begin the game?                                         \t*\n"
+	   + "* To begin a new game you need to press \"G\" key, and the new game\t*\n"
+	   + "* will begin.                                                                                        \t*\n"
+	   + "*             Q2: What is FAQ?                                                                  \t*\n"
+	   + "* The FAQ is acronim from Frequatly Asked Question or simply, commonly\t*\n"
+	   + "* asked questions that users can have.                                          \t*\n"
+	   +"*             Q3: What is recyclopedia?                                                       \t*\n"
+	   + "* The Recyclopedia is an game world encyplopedia that hold a               \t*\n"
+	   + "* a knowdlege about the game world and all necessary information that  \t*\n"
+	   + "* can be helpful in the game about the resources, game objects and many\t*\n"
+	   + "* other very important information about the world.                      \t*\n");
+   System.out.println("\\***************************************************************************/");
+   }
+}
